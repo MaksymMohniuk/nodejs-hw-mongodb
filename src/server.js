2dd2a4dc78728a117f4d1225880a5133973f6a46
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-29 11:28:19 +0300

    added-error-catching

--- src/server.js
+++ src/server.js
@@ -29,20 +29,29 @@ export const setupServer = () => {
 
   app.get('/contacts/:contactId', async (req, res) => {
     const id = req.params.contactId;
-    const contact = await getContactById(id);
 
-    if (!contact) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with ID ${id} was not found`,
+    try {
+      const contact = await getContactById(id);
+
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with ID ${id} was not found`,
+        });
+      }
+
+      res.json({
+        status: 200,
+        message: `Successfully found contact with id ${id}!`,
+        data: contact,
+      });
+    } catch (error) {
+      console.error(`Error fetching contact with ID ${id}:`, error);
+      res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
       });
     }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data: contact,
-    });
   });
 
   app.use('*', (req, res) => {
