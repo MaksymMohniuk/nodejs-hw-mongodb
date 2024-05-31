114e7e4aad7f9d59b188ae26db25e8083b5e3799
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-06-01 00:57:43 +0300

    routing&controllers-organized

---
+++ src/controllers/contacts.js
@@ -0,0 +1,37 @@
+import { getAllContacts, getContactById } from '../services/contacts';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const id = req.params.contactId;
+
+  try {
+    const contact = await getContactById(id);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact with ID ${id} was not found`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data: contact,
+    });
+  } catch (error) {
+    console.error(`Error fetching contact with ID ${id}:`, error);
+    res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+    });
+  }
+};
