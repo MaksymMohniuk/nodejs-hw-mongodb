39bfea3e8a2865428a25ecb92928e9facbe70eef
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-06-09 14:47:33 +0300

    sorting-added

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,12 +1,21 @@
 import { Contact } from '../db/Contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = 'asc',
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
   const contactsQuery = Contact.find();
   const contactsCount = await Contact.countDocuments();
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
   const paginationData = calculatePaginationData(contactsCount, page, perPage);
 
   return {
