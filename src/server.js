2c8d82318972375e11bcb40467d0f88be48f2cdb
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-07-07 00:08:15 +0300

    added-get-routes

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { errorHandler, notFoundHandler } from './middlewares/middlewares.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -23,6 +24,7 @@ export const setupServer = () => {
   );
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('api-docs', swaggerDocs());
 
   app.use(router);
 
